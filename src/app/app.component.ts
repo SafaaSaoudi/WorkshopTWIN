61e845134f0d166a335596b67a1dd4cdd79ea057
safa <safa.saoudi@esprit.tn>
2024-09-25 10:59:36 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'workshop';
+  t=20;
+ list=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
 }
